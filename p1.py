70f90dd4e0b3b4d270435b2bfd0cbdfbbc055a5d
aswin <xaswin230@mail.com>
2025-06-12 09:53:11 +0530

    python c1

---
+++ p1.py
@@ -0,0 +1,7 @@
+print("hello, world")
+a=10
+print(a)
+s="jayan"
+print(s)
+
+
---
+++ py2.py
@@ -0,0 +1,42 @@
+#with temp_________________________________
+
+# a=10
+# b=20
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+#without temp1______________________________
+
+# a=10
+# b=20
+# b=a+b
+# a=b-a
+# b=b-a
+# print(a,b)
+
+#without temp2______________________________
+
+
+# a=10
+# b=20
+# a,b=b,a
+# print(a,b)
+
+#printing format______________________________
+
+
+# a=10
+# b=20
+# a,b=b,a
+# print("a=",a,"b=",b)
+# print("a={} b={}".format(a,b))
+# print(f"a={a} b={b}")
+
+
+#data type_____________________________________
+
+a="erhfb"
+# a=10
+print(type(a))
