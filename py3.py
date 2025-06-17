9d6c9ea8df5eff86c1558977e5363850bba54a32
aswin <xaswin230@mail.com>
2025-06-17 09:30:00 +0530

    if

--- py3.py
+++ py3.py
@@ -1,7 +1,15 @@
-i=10
-if i<10:
-    print("i is less than 10")
-elif i==10:
-    print("i is not in if")
+# i=10
+# if i<10:
+#     print("i is less than 10")
+# elif i==10:
+#     print("i is not in if")
     
     
+i=20
+if (i<15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print("i is greater than 15")
+    print("i'm in else block")
+print("i is not in if or elif block")
