85d8f9014aa0c1b17fea9548851d97fc628c6a1f
aswin <xaswin230@mail.com>
2025-06-19 09:48:06 +0530

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,17 @@
+#print numbers from 1 to 5 using a while loop
+
+# count=1
+# while count <= 5:
+#     print(count)
+#     count += 1
+    
+    
+    
+#print even numbers from 1 to 10
+
+count = 1
+while count <= 10:
+    if count % 2 == 0:
+        print(count)
+    count += 1
+    
