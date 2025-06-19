47245746f3eb19eb0f5aa83cf8f1a95060c827f6
aswin <xaswin230@mail.com>
2025-06-19 10:28:13 +0530

    while

--- whileloop.py
+++ whileloop.py
@@ -9,9 +9,31 @@
     
 #print even numbers from 1 to 10
 
-count = 1
-while count <= 10:
-    if count % 2 == 0:
-        print(count)
-    count += 1
+# count = 1
+# while count <= 10:
+#     if count % 2 == 0:
+#         print(count)
+#     count += 1
     
+    
+#print numbers from 10 to 1
+
+# count=int(input("enter a number"))
+# while count >=1:
+#     print(count)
+#     count -= 1
+
+
+
+#calculator for addition of number of numbers the user enters
+# num = int(input("Enter a number: "))
+# sum = 0
+# while num != -1:
+#     sum += num
+#     num = int(input("Enter a number : "))
+# print("the sum of the number is :", sum)
+
+
+
+
+
