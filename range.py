6295fdd0eece9ea7139dda0a4b90368649772b78
aswin <xaswin230@mail.com>
2025-06-20 10:32:55 +0530

    whileee

---
+++ range.py
@@ -0,0 +1,29 @@
+# print(range(15))
+# print(list(range(15)))
+# print(list(range(4,9)))
+# print(list(range(5, 25, 4)))
+
+
+#tuple
+
+# tuple_ =("python","loops","sequence","condition","range")
+# for iterator in range(len(tuple_)):
+#     print(tuple_[iterator].upper(),end=" , ")
+
+
+
+# numbers = (4,2,6,7,3,5,8,10,6,1,9,2)
+# square = 0
+# squares = []
+# for value in numbers:
+#     square = value ** 2
+#     squares.append(square)
+# print("The list of square is:", squares)
+
+
+string ="Python Loop"
+for s in string:
+    if s=="o":
+        print("if block")
+else:
+    print(s)
--- whileloop.py
+++ whileloop.py
@@ -35,5 +35,41 @@
 
 
 
+#sum off diits
+
+# i=int(input("Enter a number: "))
+# j=0
+# while (i>0):
+#     a=i%10
+#     j=j+a
+#     i=i//10
+# print("the sum of the digits is :", j)
 
 
+#palindrome__________________
+# i = input("Enter a number: ")
+# j=0
+# b=i
+# while (b>0):
+#     a=(b%10)
+#     j=(j*10)+a
+#     b=b//10
+# if (i==j):
+#     print("the number is a palindrome")
+# else:
+#     print("the number is not a palindrome")
+
+
+#amstrong number____________________
+i= int(input("Enter a number: "))
+a=i
+j=0
+b=len(str(i))
+while (i>0):
+    b=i%10
+    f=f+b**3
+    i=i//10
+if (a==f):
+    print("the number is an amstrong number")
+else:
+    print("the number is not an amstrong number")
