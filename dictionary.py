b5f6a2846bbb80de0b90d405b63d02313a239d96
aswin <xaswin230@mail.com>
2025-07-14 10:30:51 +0530

    function

---
+++ dictionary.py
@@ -0,0 +1,43 @@
+# capital_city={"Nepal":"Kathmandu","Italy":"Rome"}
+# print(capital_city)
+# for i in capital_city:
+#     print(i)
+#     print(capital_city[i])
+
+
+# capital_city={"Nepal":"Kathmandu","England":"London"}
+# print("Initial Dictionary: ",capital_city)
+# capital_city["Japan"] = "Tokyo"
+# print("Updated Dictionary: ",capital_city)
+
+
+# student_id={111: "Eric",112:"Kyle",113:"Butters"}
+# print("Initial Dictionary: ",student_id)
+# student_id[112]="Stan"
+# print("Updated Dictionary: ",student_id)
+
+
+#access elemennt
+
+# student_id={111: "Eric",112:"Kyle",113:"Butters"}
+# print(student_id[111]) 
+# print(student_id[113]) 
+
+#removing element
+
+# student_id={111: "Eric",112:"Kyle",113:"Butters"}
+# print("Initial Dictionary: ",student_id)
+# del student_id[111]
+# print("Updated Dictionary: ",student_id)
+
+#iterating throgh a dictonary
+
+# squares={1:1,3:9,5:25,7:49,9:81}
+# for i in squares:
+#     print(squares[i])
+
+# squares={}
+# n=10
+# for n in range(0,11):
+    
+
---
+++ function.py
@@ -0,0 +1,35 @@
+#odd or even
+
+
+# def is_even(num):
+#     if num%2==0:
+#         print(num,"is even")
+#     else:
+#         print(num,"is odd")
+# a=int(input("enter the numer : "))
+# is_even(a)
+
+#factorial
+
+# def factorial(num):
+#     fact=1
+#     for i in range(1,num+1):
+#         fact=fact*i
+#     print("factorial of the number is",fact)
+# n=int(input("enter the numer"))
+# factorial(n)
+
+
+#leapyear
+
+# def leapyear(num):
+#     if (num % 4 == 0 and num % 100 != 0) or (num % 400 == 0):
+#         print(num, "is a leap year.")
+#     else:
+#         print(num, "is not a leap year.")
+# year=int(input("Enter the year"))
+# leapyear(year)
+
+
+
+
